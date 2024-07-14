070347d8aec71ce8bb039d5055e4dbe6b8666534
Hammad <hammadhabib225@gmail.com>
2024-07-14 21:53:09 +0500

    variables and console.table

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+var b = 34;
+console.log(`b: ${b}`)
+var b = 32; // declared twice thats why var is not perfered
+console.log(`b: ${b}`)
+let a = 'hello g'
+console.log(`a: ${a}`)
+a = 21
+console.log(`a: ${a}`)
+
+const c = 1234; 
+
+// you already know about variables
+// so let me tell you another intersersting command
+
+console.table([b, a, c])
